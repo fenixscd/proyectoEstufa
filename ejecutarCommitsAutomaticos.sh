ae13233b276ac0a2295f034f5cb8e1e36f6c35c9
Samuel <fenixscd@gmail.com>
2017-02-10 13:11:32 +0100

    Commit automatico 10/02/2017 -- 13:11

---
+++ ejecutarCommitsAutomaticos.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#Ejecutar commits automaticos
+
+
+function barraDeProgreso(){
+
+	local fraccion=`expr $1 / 10`
+
+	echo -ne '[......................] (0%)\r'
+	sleep  $fraccion
+	echo -ne '[##....................] (10%)\r'
+	sleep  $fraccion
+	echo -ne '[####..................] (20%)\r'
+	sleep  $fraccion
+	echo -ne '[######................] (30%)\r'
+	sleep  $fraccion
+	echo -ne '[########..............] (40%)\r'
+	sleep  $fraccion
+	echo -ne '[##########............] (50%)\r'
+	sleep  $fraccion
+	echo -ne '[############..........] (60%)\r'
+	sleep  $fraccion
+	echo -ne '[##############........] (70%)\r'
+	sleep  $fraccion
+	echo -ne '[################......] (80%)\r'
+	sleep  $fraccion
+	echo -ne '[####################..] (90%)\r'
+	sleep  $fraccion
+	echo -ne '[######################] (100%)\r'
+}
+
+clear
+while [ true ]
+do
+  DIA=`date +"%d/%m/%Y"`
+  HORA=`date +"%H:%M"`
+  echo "Hoy es el $DIA y la hora actual es $HORA!"
+	clear
+  git add .
+  git commit -m "Commit automatico $DIA -- $HORA"
+	echo ''
+	barraDeProgreso 10
+done
