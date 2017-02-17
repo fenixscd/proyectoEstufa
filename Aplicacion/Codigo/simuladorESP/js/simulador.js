6cf0f94065e536777775168c5535672767b5af0c
Samuel <fenixscd@gmail.com>
2017-02-17 12:28:19 +0100

    He empezado con la parte de js

--- Aplicacion/Codigo/simuladorESP/js/simulador.js
+++ Aplicacion/Codigo/simuladorESP/js/simulador.js
@@ -1,6 +1,2 @@
-//Generar numeros aleatorios
-// Retorna un número aleatorio entre min (incluido) y max (excluido)
-function getRandomArbitrary(min, max) {
-//  return Math.random() * (max - min) + min;
-    return min + max;
-}
+modo = document.getElementById("modo");
+estadoConexion = document.getElementById("connected");
