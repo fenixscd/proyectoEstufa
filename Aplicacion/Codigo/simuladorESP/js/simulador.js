88e47fe33a686dd12ae692050aec76350f170534
Samuel <fenixscd@gmail.com>
2017-02-17 18:41:26 +0100

    Un poco atascasdo con el random

--- Aplicacion/Codigo/simuladorESP/js/simulador.js
+++ Aplicacion/Codigo/simuladorESP/js/simulador.js
@@ -18,45 +18,58 @@ hora2Temp = document.getElementById("hora2Temp");
 
 //kk
 
-console.log(dispositivo.innerHTML) 
-console.log(modo.innerHTML)
-console.log(conexion.innerHTML)
-console.log(temp.innerHTML)
-console.log(hume.innerHTML)
+function valoresDisplay (){
+    console.log(dispositivo.innerHTML) 
+    console.log(modo.innerHTML)
+    console.log(conexion.innerHTML)
 
-console.log(resis1Esta.innerHTML)
-console.log(resis1Temp.innerHTML)
-console.log(hora1.innerHTML)
-console.log(hora1Cambio.innerHTML)
-console.log(hora1Temp.innerHTML)
+    console.log(temp.innerHTML)
+    console.log(hume.innerHTML)
 
-console.log(resis2Esta.innerHTML)
-console.log(resis2Temp.innerHTML)
-console.log(hora2.innerHTML)
-console.log(hora2Cambio.innerHTML)
-console.log(hora2Temp.innerHTML)
+    console.log(resis1Esta.innerHTML)
+    console.log(resis1Temp.innerHTML)
+    console.log(hora1.innerHTML)
+    console.log(hora1Cambio.innerHTML)
+    console.log(hora1Temp.innerHTML)
 
-
-// Retorna un número aleatorio entre min (incluido) y max (excluido)
-//function getRandomArbitrary(min, max) {
-//  return Math.random() * (max - min) + min;
-//}
-
-function wait(nsegundos) {
-objetivo = (new Date()).getTime() + 1000 * Math.abs(nsegundos);
-while ( (new Date()).getTime() < objetivo );
-};
-
-//do {
-//    console.log("Dentro del bucle");
-//    wait(5);
-//} while (true);
-
-
-function enviarBucle(){
-    window.setTimeout(enviarBucle, 2000);
-    console.log("Dentro del bucle");
+    console.log(resis2Esta.innerHTML)
+    console.log(resis2Temp.innerHTML)
+    console.log(hora2.innerHTML)
+    console.log(hora2Cambio.innerHTML)
+    console.log(hora2Temp.innerHTML)
 }
 
 
+
+// Retorna un número aleatorio entre min (incluido) y max (excluido)
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function gerarValores(valor){
+    var numPosibilidades = (valor+1) - (valor-1) 
+   	var aleat = Math.random() * numPosibilidades
+    console.log("Antes del round: " + aleat);
+   	aleat = Math.round(aleat);
+    console.log("Aleatorio: " + aleat);
+//    parseFloat
+//   	return parseInt(inferior) + aleat 
+}
+
+function generarTemperatura(){
+    temperatura = temp.innerHTML;
+    temperatura = getRandomArbitrary (temperatura, (temperatura+1));
+}
+
+function generarHumedad(){
+    humedad =  hume.innerHTML;
+    humedad = getRandomArbitrary (humedad, (humedad+1));
+}
+
+medicion = 25;
+function enviarBucle(){
+    window.setTimeout(enviarBucle, 1000);
+    //console.log("Medicion: " +gerarValores(medicion));
+    gerarValores(medicion);
+}
 enviarBucle();
