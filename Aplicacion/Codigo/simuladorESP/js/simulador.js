5fd765a46a230dc59cdf01e1fcb521265d2ba820
Samuel <fenixscd@gmail.com>
2017-02-17 13:09:47 +0100

    Ya capturo todos los datos del html para el js

--- Aplicacion/Codigo/simuladorESP/js/simulador.js
+++ Aplicacion/Codigo/simuladorESP/js/simulador.js
@@ -1,26 +1,39 @@
+dispositivo = document.getElementById("dispositivo");
 modo = document.getElementById("modo");
-estadoConexion = document.getElementById("connected");
+conexion = document.getElementById("conexion");
 temp = document.getElementById("temp");
 hume = document.getElementById("hume");
 
 resis1Esta = document.getElementById("resis1Esta");
-resis1Temp = document.getElementById("resis1temp");
+resis1Temp = document.getElementById("resis1Temp");
 hora1 = document.getElementById("hora1");
 hora1Cambio = document.getElementById("hora1Cambio");
-hora1Cambio = document.getElementById("hora1Cambio");
+hora1Temp = document.getElementById("hora1Temp");
 
 resis2Esta = document.getElementById("resis2Esta");
 resis2Temp = document.getElementById("resis2Temp");
 hora2 = document.getElementById("hora2");
 hora2Cambio = document.getElementById("hora2Cambio");
- = document.getElementById("");
- = document.getElementById("");
- = document.getElementById("");
- = document.getElementById("");
- = document.getElementById("");
- = document.getElementById("");
- = document.getElementById("");
- = document.getElementById("");
+hora2Temp = document.getElementById("hora2Temp");
 
+//kk
+
+console.log(dispositivo.innerHTML) 
+console.log(modo.innerHTML)
+console.log(conexion.innerHTML)
+console.log(temp.innerHTML)
+console.log(hume.innerHTML)
+
+console.log(resis1Esta.innerHTML)
+console.log(resis1Temp.innerHTML)
+console.log(hora1.innerHTML)
+console.log(hora1Cambio.innerHTML)
+console.log(hora1Temp.innerHTML)
+
+console.log(resis2Esta.innerHTML)
+console.log(resis2Temp.innerHTML)
+console.log(hora2.innerHTML)
+console.log(hora2Cambio.innerHTML)
+console.log(hora2Temp.innerHTML)
 
 
