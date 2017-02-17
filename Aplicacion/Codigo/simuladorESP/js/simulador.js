51842c2b5d08d352a6bb8a772022b264c548d4d1
Samuel <fenixscd@gmail.com>
2017-02-17 12:48:55 +0100

    Continuo con la rvinculación del html con el js

--- Aplicacion/Codigo/simuladorESP/js/simulador.js
+++ Aplicacion/Codigo/simuladorESP/js/simulador.js
@@ -1,2 +1,26 @@
 modo = document.getElementById("modo");
 estadoConexion = document.getElementById("connected");
+temp = document.getElementById("temp");
+hume = document.getElementById("hume");
+
+resis1Esta = document.getElementById("resis1Esta");
+resis1Temp = document.getElementById("resis1temp");
+hora1 = document.getElementById("hora1");
+hora1Cambio = document.getElementById("hora1Cambio");
+hora1Cambio = document.getElementById("hora1Cambio");
+
+resis2Esta = document.getElementById("resis2Esta");
+resis2Temp = document.getElementById("resis2Temp");
+hora2 = document.getElementById("hora2");
+hora2Cambio = document.getElementById("hora2Cambio");
+ = document.getElementById("");
+ = document.getElementById("");
+ = document.getElementById("");
+ = document.getElementById("");
+ = document.getElementById("");
+ = document.getElementById("");
+ = document.getElementById("");
+ = document.getElementById("");
+
+
+
