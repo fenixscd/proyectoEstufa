bcd30278da4530fc2f1255dcc44c674756404e1d
Samuel <fenixscd@gmail.com>
2017-02-17 12:08:01 +0100

    Modficando un poco el diseño tambien

---
+++ Aplicacion/Codigo/simuladorESP/js/simulador.js
@@ -0,0 +1,6 @@
+//Generar numeros aleatorios
+// Retorna un número aleatorio entre min (incluido) y max (excluido)
+function getRandomArbitrary(min, max) {
+//  return Math.random() * (max - min) + min;
+    return min + max;
+}
