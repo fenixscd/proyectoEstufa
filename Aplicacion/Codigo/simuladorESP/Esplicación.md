092763201252a8438e4929a56fdde3b5ad740f26
Samuel <fenixscd@gmail.com>
2017-02-22 07:18:22 +0100

    Ya puedo agregar displey

--- Aplicacion/Codigo/simuladorESP/Esplicación.md
+++ Aplicacion/Codigo/simuladorESP/Esplicación.md
@@ -3,3 +3,59 @@
 El método startsWith() indica si un string inicia con los caracteres de otro string, regresando true o false según sea el caso. [Url](https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/startsWith)
 `String.prototype.startsWith != "function"`
 
+
+
+
+
+var rojos = document.getElementsByClassName("rojo");
+for (var i = 0; i<rojos.length; i++) {
+   rojos[i].classList.remove("rojo");
+   rojos[i].classList.add("verde");
+}
+
+--------
+
+var miElto = document.getElementsByClassName("rojo")[0];
+miElto.className = "verde";
+y el primer elemento localizado pasaría de color rojo a verde.
+
+Sin embargo, esto presenta un grave problema: si el elemento tenía más clases aplicadas además de "rojo", las pierde. el motivo es que ahora le estamos asignando como única clase aplicada "verde".
+
+-------
+
+document.getElementsByTagName("H1")[0].setAttribute("class", "democlass");
+
+------
+
+Elemina el elemento
+var elemento = document.getElementById("temp");
+elemento.parentNode.removeChild(elemento);
+
+
+-----
+
+elemento.id ="Cambio";
+
+
+
+
+## Recursos
+http://www.jasoft.org/Blog/post/La-manera-correcta-de-anadir-y-quitar-clases-CSS-a-elementos-mediante-JavaScript-puro.aspx
+http://www.w3big.com/es/js/js-htmldom-elements.html
+https://developer.mozilla.org/es/docs/Web/API/Document/createElement
+https://abalozz.es/plantillas-en-javascript-con-es2015/
+http://www.etnassoft.com/2016/10/05/template-strings-en-es6-estudiando-las-nuevas-plantillas-de-cadena-en-javascript/
+
+
+
+
+
+
+
+
+--------
+
+
+
+
+
--- Aplicacion/Codigo/simuladorESP/js/simulador.js
+++ Aplicacion/Codigo/simuladorESP/js/simulador.js
@@ -17,6 +17,18 @@ var hora2Cambio = document.getElementById("hora2Cambio");
 var hora2Temp = document.getElementById("hora2Temp");
 
 
+
+function ListaDispositivos(){
+    var listaDispositivos = new Array();
+}
+
+ListaDispositivos.prototype.genrarlistaDispositivos = function(numero){
+    for(var i = 0; i < numero; i++) {
+        listaDispositivos.push(new estufa(i));
+    }
+}
+
+
 function Resistencia(){
     this.nombre;
     this.estado;
@@ -25,19 +37,27 @@ function Resistencia(){
     this.HoraTemperatura;
 }
 
-Resitencia.prototype.setNombre = function(nombre){
+Resistencia.prototype.setNombre = function(nombre){
     this.nombre = nombre;
 }
 
 
-function estufa (){
+function Estufa (){
     this.dispositivo; // Nombre
     this.mac;
     this.conexion;
     this.temperatura;
     this.hemedad;
-    this.listaResitencias = [];
+    this.listaResitencias = new Array();
 }
+
+
+
+
+
+
+
+
 
 
 function valoresDisplay (){
@@ -75,12 +95,12 @@ function generarValores(valor, suvir){
     return calculado.toFixed(1);
 }
 
-function aumetarTemperatura {
+function aumetarTemperatura() {
     temperatura = temp.innerHTML;
     return generarValores(temperatura, true);
 }
 
-function disminuirTemperatura {
+function disminuirTemperatura() {
     temperatura = temp.innerHTML;
     return generarValores(temperatura, false);
 }
@@ -112,3 +132,85 @@ function enviarBucle(){
     //console.log("Medicion: " +gerarValores(medicion));
     gerarValores(medicion);
 }
+
+
+function renombrarID(){
+    console.log(temp.innerHTML);
+    temp.innerHTML = "30";
+    console.log(temp.innerHTML);
+    temp.id = "otro";
+    //console.log(temp.innerHTML);
+    var otro = document.getElementById("otro");
+    console.log(otro.innerHTML);
+    otro.innerHTML = "35";
+    console.log(otro.innerHTML);
+}
+
+
+// crea un nuevo div 
+  // y añade contenido 
+  
+//  var newContent = document.createTextNode("Hola!¿Qué tal?"); 
+//  newDiv.appendChild(newContent); //añade texto al div creado. 
+//
+//  // añade el elemento creado y su contenido al DOM 
+//  var currentDiv = document.getElementById("div1"); 
+//  document.body.insertBefore(newDiv, currentDiv); 
+
+
+
+function crearDisplay(mac){
+    var contenedor = document.getElementsByTagName("section")[0];
+    var dispositivo = document.createElement("div");
+    dispositivo.setAttribute("class", "dispositivo");
+    var dato = "hola";
+    
+    
+    
+    var contenido =`<div class="display">
+                        <div class="lSuperior">
+                            <h2><span id="dispositivo${mac}">ESTUFA HABITACI</span></h2>
+                            <p><span id="modo${mac}">Auto</span> | <span id="conexion${mac}">DESCONEC</span></p>
+                        </div>
+              
+                        <div class="medidas">
+                            <h2>T: <span id="temp${mac}">40</span>º</h2>
+                            <h2>H: <span id="hume${mac}">10</span>%</h2>
+                        </div>
+              
+                        <div class="programacion">
+                            <p class="izq">Resis 1: <span id="resis1Esta${mac}">ON</span> <span id="resis1Temp${mac}">27</span>º</p>  
+                            <p class="der">Hora <span id="hora1${mac}">6:25</span> <span id="hora1Cambio${mac}">OFF</span> <span id="hora1Temp${mac}">27</span>º</p>
+                  
+                            <p class="izq">Resis 2: <span id="resis2Esta${mac}">OFF</span> <span id="resis2Temp${mac}"></span></p>  
+                            <p class="der">Hora <span id="hora2${mac}">6:25</span> <span id="hora2Cambio${mac}">ON</span> <span id="hora2Temp${mac}">27</span>º</p>
+                        </div>
+               
+                    </div>
+                </div>
+                <div class="botonera">
+                <form>
+                    <input type="button" value="+" onclick="hacer_click()"/>
+                    <input type="button" value="M" onclick="hacer_click()"/>
+                    <input type="button" value="-" onclick="hacer_click()"/>
+                </form>`;
+    dispositivo.innerHTML = contenido;
+    dispositivo.classList.add("dispositivo");
+    contenedor.appendChild(dispositivo);
+    
+    
+    
+    var temperatura = document.getElementById("temp"+mac);
+    temperatura.innerHTML;
+    console.log("Temperatura del dipositivo temp" + mac + ": " + temperatura.innerHTML);
+}
+
+
+crearDisplay(12);
+
+
+
+
+
+
+
