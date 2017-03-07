ca7a600806d9df4cc152989c42586716a860282f
Samuel <fenixscd@gmail.com>
2017-03-07 01:08:54 +0100

    He sacado las variables que hay que pintar

--- Aplicacion/Codigo/simuladorESP/js/Union.js
+++ Aplicacion/Codigo/simuladorESP/js/Union.js
@@ -1,6 +1,34 @@
 function Union() {
 
 }
-Union.prototype. = function () {
+Union.prototype. = function (mac) {
+  this.mac = mac;
+  this.plantilla = '<div class="display">' +
+                      '<div class="lSuperior">' +
+                        '<h2><span id="dispositivo{{mac}}">{{mac}}</span></h2>' +
+                        '<p><span id="modo{{mac}}">Manual</span> | <span id="conexion{{mac}}">DESCONEC</span></p>' +
+                      '</div>' +
+                      '<div class="medidas">' +
+                        '<h2>T: <span id="temp{{mac}}">25.5</span>º</h2>' +
+                        '<h2>H: <span id="hume{{mac}}">10</span>%</h2>' +
+                      '</div>' +
+                      '<div class="programacion">' +
+                        '<p class="izq">Resis 1 [<span id="resis1encendida{{mac}}">*</span>]: <span id="resis1Esta{{mac}}">ON</span> <span id="resis1Temp{{mac}}">27</span>º</p>' +
+                        '<p class="der">Hora <span id="hora1{{mac}}">6:25</span> <span id="hora1Cambio{{mac}}">OFF</span> <span id="hora1Temp{{mac}}">27</span>º</p>' +
+                        '<p class="izq">Resis 2 [<span id="resis2encendida{{mac}}"> </span>]: <span id="resis2Esta{{mac}}">OFF</span> <span id="resis2Temp{{mac}}"></span></p>' +
+                        '<p class="der">Hora <span id="hora2{{mac}}">6:25</span> <span id="hora2Cambio{{mac}}">ON</span> <span id="hora2Temp{{mac}}">27</span>º</p>' +
+                      '</div>' +
+                      '</div>' +
+                      '</div>' +
+                      '<div class="botonera">' +
+                        '<form>' +
+                          '<input type="button" value="+" onclick="hacer_click()"/>' +
+                          '<input type="button" value="M" onclick="hacer_click()"/>' +
+                          '<input type="button" value="-" onclick="hacer_click()"/>' +
+                          '</form>';
+  this.plantilla();
+};
 
+Union.prototype.montarPlantilla = function () {
+  this.plantilla = this.plantilla.replace(/{{mac}}/g, this.mac);
 };
--- Aplicacion/Codigo/simuladorESP/js/variables.js
+++ Aplicacion/Codigo/simuladorESP/js/variables.js
@@ -4,20 +4,16 @@ var plantilla =
             modo{{mac}}
             conexion{{mac}}
             temp{{mac}}
-            '<h2>H: <span id="hume{{mac}}">10</span>%</h2>' +
-        '</div>' +
-        '<div class="programacion">' +
-            '<p class="izq">Resis 1 [<span id="resis1encendida{{mac}}">*</span>]: <span id="resis1Esta{{mac}}">ON</span> <span id="resis1Temp{{mac}}">27</span>º</p>' +
-            '<p class="der">Hora <span id="hora1{{mac}}">6:25</span> <span id="hora1Cambio{{mac}}">OFF</span> <span id="hora1Temp{{mac}}">27</span>º</p>' +
-
-            '<p class="izq">Resis 2 [<span id="resis2encendida{{mac}}"> </span>]: <span id="resis2Esta{{mac}}">OFF</span> <span id="resis2Temp{{mac}}"></span></p>' +
-            '<p class="der">Hora <span id="hora2{{mac}}">6:25</span> <span id="hora2Cambio{{mac}}">ON</span> <span id="hora2Temp{{mac}}">27</span>º</p>' +
-        '</div>' +
-    '</div>' +
-'</div>' +
-'<div class="botonera">' +
-    '<form>' +
-        '<input type="button" value="+" onclick="hacer_click()"/>' +
-        '<input type="button" value="M" onclick="hacer_click()"/>' +
-        '<input type="button" value="-" onclick="hacer_click()"/>' +
-    '</form>';
+            hume{{mac}}
+            resis1encendida{{mac}}
+            resis1Esta{{mac}}
+            resis1Temp{{mac}}
+            hora1{{mac}}
+            hora1Cambio{{mac}}
+            hora1Temp{{mac}}
+            resis2encendida{{mac}}
+            resis2Esta{{mac}}
+            resis2Temp{{mac}}
+            hora2{{mac}}
+            hora2Cambio{{mac}}
+            hora2Temp{{mac}}
