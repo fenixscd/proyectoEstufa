6265ff2162077e69e573e30b90e390c41831d962
Samuel Castro Dominguez <fenixscd@gmail.com>
2017-03-13 09:44:22 +0100

    En principio la resistencia esta finalizada

--- Aplicacion/Codigo/simuladorESP/js2/Resistencia.js
+++ Aplicacion/Codigo/simuladorESP/js2/Resistencia.js
@@ -1,8 +1,9 @@
-function Resitencia (){
+function Resitencia (encendida, automatico, temperatura, temperaturaActual){
   this.encendida   = false; // Si tiene que estar en modo endendida
   this.automatico  = false; // Si esta en modo automatico realiza las hoperaciones con la hora si no no lo hace
   this.temperatura = false; // Temperatura de funcionamiento;
   this.estado 	   = false; // Encendida cuando la temperatura no es la soliciata apagada si la temperatura es igula o superior a al necesitada.
+  this.cambiarValores(encendida, automatico, temperatura, temperaturaActual);
 }
 
 
@@ -22,17 +23,51 @@ Resitencia.prototype.getEstado = function() {
 	return this.estado;
 };
 
+// Seters --------
 
-
-
-
-
-Resitencia.prototype.isEqualsTeperatura = function() {
-	// Verificamos que la temperatura es igual o superior a la especificada
-	// Si es igula o superior la apagamos y si no la encendemos
-
+Resitencia.prototype.setEncendida = function(encendida) {
+	this.encendida = encendida;
 };
 
-Resitencia.prototype.cambiarEstado = function() {
-	// Cambio el estado y reseteo los valores a cero
+Resitencia.prototype.setAutomatico = function(automatico) {
+	this.automatico = automatico;
 };
+
+Resitencia.prototype.setTemperatura = function(temperatura) {
+	this.temperatura = temperatura;
+};
+
+Resitencia.prototype.setEstado = function(estado) {
+	this.estado = estado;
+};
+
+Resitencia.prototype.actualizarEstado = function(temperaturaActual) {
+	if (this.temperatura >== temperaturaActual){
+		this.setEstado(false);
+	} else {
+		this.setEstado(true);
+	}
+};
+
+
+/////////////// METODOS QUE SE UTILIZARAN DESDE FUERA ///////////////////////
+
+Resitencia.prototype.cambiarValores = function(encendida, automatico, temperatura, temperaturaActual) {
+	this.getEncendida(encendida);
+	this.getAutomatico(automatico);
+	this.temperatura(temperatura);
+
+	this.actualizarEstado();
+};
+
+Resitencia.prototype.getJSON = function() {
+	return{
+    	"encendida":this.getEncendida(),
+		"automatico":this.getAutomatico(),
+		"temperatura":this.getTemperatura(),
+		"estado":this.getEstado()
+  };
+};
+
+
+
