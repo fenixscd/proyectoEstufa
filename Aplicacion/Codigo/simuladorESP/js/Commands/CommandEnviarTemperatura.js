18e0b4b4ec97fd16332b50d1fd84b18c2b552a76
Samuel <fenixscd@gmail.com>
2017-04-21 22:25:37 +0200

    Estoy vastante satisfecho por ahora con el servidor

--- Aplicacion/Codigo/simuladorESP/js/Commands/CommandEnviarTemperatura.js
+++ Aplicacion/Codigo/simuladorESP/js/Commands/CommandEnviarTemperatura.js
@@ -10,13 +10,12 @@ CommandEnviarTemperatura.prototype.getNombre = function () {
 
 CommandEnviarTemperatura.prototype.ejecutar = function(parametros) {
   // Crear todo lo necesario para la contestacion en la lista de dispositivo
-  this.comun();
+
+  if (parametros.codigoEnvio == undefined){
+    parametros.codigoEnvio = "Sin Codigo de Envio";
+  }
+  console.log("Codigo Envio " + parametros.codigoEnvio);
+  this.datos.mac = this.dispositivo.mac;
   this.datos.respuesta = this.dispositivo.termometro.getMedicion();
   this.dispositivo.conexion.enviarMensaje(JSON.stringify(this.datos));
-  parametros;
-};
-
-CommandEnviarTemperatura.prototype.comun = function () {
-  this.datos.tipoDispositivo = this.dispositivo.getTipoDispositivo();
-  this.datos.mac = this.dispositivo.mac;
 };
