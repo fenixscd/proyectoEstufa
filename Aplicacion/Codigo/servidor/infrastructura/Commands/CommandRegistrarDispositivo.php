6b5e32ae83121dca6e35ce7fcf30f3cfcbdf54b3
Samuel <fenixscd@gmail.com>
2017-04-22 12:49:10 +0200

    Problemas con los archivos por no cambiar de la rama master

---
+++ Aplicacion/Codigo/servidor/infrastructura/Commands/CommandRegistrarDispositivo.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace infrastructura\Commands;
+
+use infrastructura\Dispositivo;
+use infrastructura\ListaDispositivos;
+
+class CommandRegistrarDispositivo{
+  private $nombre;
+  private $listaDispositivos;
+
+  public function __construct($listaDispositivos){
+    $this->nombre            = "registrarDispositivo";
+    $this->listaDispositivos = $listaDispositivos;
+  }
+
+  public function getNombre(){
+    return $this->nombre;
+  }
+
+  public function ejecutar($conec, $parametros){
+
+    if(!$this->listaDispositivos->isExistsMac($parametros["mac"])){
+      $dispositivo = new Dispositivo();
+      $dispositivo->setMac($parametros["mac"])
+                  ->setConexionDispositivo($conec);
+      $this->listaDispositivos->addDispositivo($dispositivo);
+    }else {
+      $this->listaDispositivos->
+    }
+
+    echo "-Commando registrarDispositivo";
+  }
+
+}
